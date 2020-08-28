40a32185c5367a2a35b0c2e54335dd66b30653a9
kdesnos <kdesnos@insa-rennes.fr>
2020-08-28 09:21:28 +0200

    (Releng) Complement build.sh and ignore changes in gitmodule

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ cd build
 sudo apt-get install libsdl1.2-dev libsdl-gfx1.2-dev libsdl-image1.2-dev cmake
 cmake -DUSE_SDL=ON -DUSE_RLGLUE=OFF -DBUILD_EXAMPLES=ON ..
 make 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
 cd ../..
 
 export TPGPATH=$(pwd)
